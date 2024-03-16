4acf3a1101349958bced0bd93b1117e03be92547
sc-zhang <zsc-zhang@foxmail.com>
2024-03-17 03:40:38 +0800

    update UI, move work thread to new thread to avoid main form freeze

--- pop_cnv/worker/gui_pipeline.py
+++ pop_cnv/worker/gui_pipeline.py
@@ -1,15 +1,35 @@
-from os import path
-import sys
+from os import path, listdir
+from multiprocessing import cpu_count
 from pop_cnv.ui import ui_popcnv_main_form
+from pop_cnv.io import loader
+from pop_cnv.worker.gui_popcnv_worker import Worker
 from PySide6.QtWidgets import QWidget, QFileDialog, QMessageBox
-from PySide6.QtCore import QCoreApplication, QEventLoop
+from PySide6.QtCore import QThread, Signal
+from traceback import format_exc
+import gzip
 
 
 class GPopCNV(QWidget):
+    __run_signal = Signal(list)
+
     def __init__(self):
         super(GPopCNV, self).__init__()
         self.ui = None
         self.graph_scene = None
+
+        # variables for popCNV
+        self.__grp_db = {}
+        self.__genome_file = ""
+        self.__mosdepth_dir = ""
+        self.__seq_depth_file = ""
+        self.__smp_file = ""
+        self.__gene_list_file = ""
+        self.__win_size = 0
+        self.__wrkdir = ""
+        self.__threads = 0
+
+        self.__worker = Worker()
+        self.__worker_thread = QThread()
 
         self.__init_ui()
 
@@ -21,3 +41,144 @@ class GPopCNV(QWidget):
         self.ui.grpSettings.setEnabled(False)
         self.ui.btn_run.setEnabled(False)
         self.ui.grpOutput.setEnabled(False)
+
+        # Set button connects
+        self.ui.btn_load_genome.clicked.connect(self.__get_genome_path)
+        self.ui.btn_load_seqdepth.clicked.connect(self.__get_seqdepth_path)
+        self.ui.btn_load_mosdepth.clicked.connect(self.__get_mosdepth_path)
+        self.ui.btn_load_smp_list.clicked.connect(self.__get_smplist_path)
+        self.ui.btn_load_gene_list.clicked.connect(self.__get_genelist_path)
+        self.ui.btn_load_wrkdir.clicked.connect(self.__get_work_path)
+
+        self.ui.btn_check.clicked.connect(self.__check_data)
+        self.ui.btn_run.clicked.connect(self.__run_popcnv)
+
+    def __get_genome_path(self):
+        genome_file_path = QFileDialog.getOpenFileName(self, "Load Genome")[0]
+        if genome_file_path:
+            self.ui.text_genome_file_path.setText(genome_file_path)
+
+    def __get_mosdepth_path(self):
+        mosdepth_path = QFileDialog.getExistingDirectory(self, "Load Mosdepth")
+        if mosdepth_path:
+            self.ui.text_mosdepth_folder_path.setText(mosdepth_path)
+
+    def __get_seqdepth_path(self):
+        seqdepth_path = QFileDialog.getOpenFileName(self, "Load Seq depth")[0]
+        if seqdepth_path:
+            self.ui.text_seq_depth_file_path.setText(seqdepth_path)
+
+    def __get_smplist_path(self):
+        smplist_path = QFileDialog.getOpenFileName(self, "Load sample list")[0]
+        if smplist_path:
+            self.ui.text_smp_list_file_path.setText(smplist_path)
+
+    def __get_genelist_path(self):
+        genelist_path = QFileDialog.getOpenFileName(self, "Load gene list")[0]
+        if genelist_path:
+            self.ui.text_gene_list_file_path.setText(genelist_path)
+
+    def __get_work_path(self):
+        work_path = QFileDialog.getExistingDirectory(self, "Select work directory")
+        if work_path:
+            self.ui.text_wrkdir.setText(work_path)
+
+    def __notify_with_title(self, info=""):
+        if info:
+            self.setWindowTitle("popCNV - %s" % info)
+        else:
+            self.setWindowTitle("popCNV")
+
+    def __check_data(self):
+        self.__genome_file = self.ui.text_genome_file_path.text()
+        self.__mosdepth_dir = self.ui.text_mosdepth_folder_path.text()
+        self.__seq_depth_file = self.ui.text_seq_depth_file_path.text()
+        self.__smp_file = self.ui.text_smp_list_file_path.text()
+        self.__gene_list_file = self.ui.text_gene_list_file_path.text()
+
+        if not self.__genome_file or not path.isfile(self.__genome_file):
+            QMessageBox.critical(self, "Loading error", "Genome file not found")
+            return
+
+        if not self.__mosdepth_dir or not path.isdir(self.__mosdepth_dir):
+            QMessageBox.critical(self, "Loading error", "Mosdepth folder not found")
+            return
+
+        if not self.__seq_depth_file or not path.isfile(self.__seq_depth_file):
+            QMessageBox.critical(self, "Loading error", "Seq depth file not found")
+            return
+
+        if not self.__smp_file or not path.isfile(self.__smp_file):
+            QMessageBox.critical(self, "Loading error", "Sample list not found")
+            return
+
+        if not self.__gene_list_file or not path.isfile(self.__gene_list_file):
+            QMessageBox.critical(self, "Loading error", "Gene list not found")
+            return
+
+        # Set threads list
+        threads_list = [str(_) for _ in range(1, cpu_count() + 1)]
+        self.ui.cbox_threads.addItems(threads_list)
+        self.ui.cbox_threads.setCurrentText(threads_list[-1])
+
+        # Load sample list
+        self.__notify_with_title("Loading samples")
+        try:
+            grp_loader = loader.GRPLoader()
+            grp_loader.load(self.__smp_file)
+            self.__grp_db = grp_loader.grp_db
+            grp_set = set(self.__grp_db[_] for _ in self.__grp_db)
+            self.ui.cbox_wild_group.addItems(grp_set)
+            self.__notify_with_title()
+        except Exception as e:
+            QMessageBox.critical(self, "Loading failed", format_exc())
+            self.__notify_with_title(" Failed with: %s" % repr(e))
+            return
+
+        # Enable controls
+        self.ui.grpSettings.setEnabled(True)
+        self.ui.btn_run.setEnabled(True)
+
+    def __get_win(self):
+        for fn in listdir(self.__mosdepth_dir):
+            if not fn.endswith(".regions.bed.gz"):
+                continue
+            mos_file = path.join(self.__mosdepth_dir, fn)
+            with gzip.open(mos_file, 'rt') as fin:
+                for line in fin:
+                    data = line.strip().split()
+                    sp = int(data[1])
+                    ep = int(data[2])
+                    return ep - sp
+
+    def __popcnv_completed(self, v):
+        self.__worker_thread.quit()
+        self.ui.btn_run.setEnabled(True)
+
+    def __run_popcnv(self):
+        # Check workdir available
+        self.__wild_group = self.ui.cbox_wild_group.currentText()
+        self.__wrkdir = self.ui.text_wrkdir.text()
+        self.__threads = int(self.ui.cbox_threads.currentText())
+        if not self.__wrkdir or not path.isdir(self.__wrkdir):
+            QMessageBox.critical(self, "Running error", "Work directory not found")
+            return
+
+        self.ui.btn_run.setEnabled(False)
+        self.__win_size = self.__get_win()
+        self.__worker.progress.connect(self.__notify_with_title)
+        self.__worker.completed.connect(self.__popcnv_completed)
+        self.__worker.set_param(self,
+                                self.__genome_file,
+                                self.__mosdepth_dir,
+                                self.__seq_depth_file,
+                                self.__smp_file,
+                                self.__gene_list_file,
+                                self.__win_size,
+                                self.__wrkdir,
+                                self.__wild_group,
+                                self.__threads)
+        self.__run_signal.connect(self.__worker.run_popcnv)
+        self.__worker.moveToThread(self.__worker_thread)
+        self.__run_signal.emit(1)
+        self.__worker_thread.start()
---
+++ pop_cnv/worker/gui_popcnv_worker.py
@@ -0,0 +1,176 @@
+from PySide6.QtWidgets import QMessageBox
+from PySide6.QtCore import QObject, Signal
+from os import path, listdir
+from pop_cnv.worker import calculator
+from pop_cnv.io import loader, writer
+from traceback import format_exc
+
+
+class Worker(QObject):
+    progress = Signal(str)
+    completed = Signal(int)
+
+    def __init__(self):
+        super().__init__()
+        self.__parent_form = None
+        self.__genome_file = None
+        self.__mosdepth_dir = None
+        self.__seq_depth_file = None
+        self.__smp_file = None
+        self.__gene_list_file = None
+        self.__win_size = None
+        self.__wrkdir = None
+        self.__wild_group = None
+        self.__threads = None
+
+    def set_param(self,
+                  parent_form,
+                  genome_file,
+                  mosdepth_dir,
+                  seq_depth_file,
+                  smp_file,
+                  gene_list_file,
+                  win_size,
+                  wrkdir,
+                  wild_group,
+                  threads):
+        self.__parent_form = parent_form
+        self.__genome_file = genome_file
+        self.__mosdepth_dir = mosdepth_dir
+        self.__seq_depth_file = seq_depth_file
+        self.__smp_file = smp_file
+        self.__gene_list_file = gene_list_file
+        self.__win_size = win_size
+        self.__wrkdir = wrkdir
+        self.__wild_group = wild_group
+        self.__threads = threads
+
+    def run_popcnv(self, v):
+        # Step 01
+        self.progress.emit("GC statistics")
+
+        gc_file = path.join(self.__wrkdir, "01.gc.txt")
+        if not path.exists(gc_file):
+            try:
+                gc_runner = calculator.GC()
+                gc_runner.stat(self.__genome_file, self.__win_size, self.__threads)
+                gc_db = gc_runner.gc_db
+                gc_writer = writer.GCWriter(gc_file)
+                gc_writer.write(gc_db)
+            except Exception as e:
+                QMessageBox.critical(self.__parent_form, "GC statistics failed", format_exc())
+                self.progress.emit(" Failed with: %s" % repr(e))
+                return
+        self.progress.emit("")
+
+        # Step 02
+        self.progress.emit("Loading sequence depth of samples")
+        try:
+            sd_loader = loader.DepthLoader()
+            sd_loader.load(self.__seq_depth_file)
+            sd_db = sd_loader.depth_db
+        except Exception as e:
+            QMessageBox.critical(self.__parent_form, "Sequence depth load failed", format_exc())
+            self.progress.emit(" Failed with: %s" % repr(e))
+            return
+        self.progress.emit("")
+
+        # Step 03
+        rd_file = path.join(self.__wrkdir, "02.rd.txt")
+        cn_file = path.join(self.__wrkdir, "03.cn.txt")
+        rd_db = {}
+        self.progress.emit("Calculating read depth")
+        if not path.exists(rd_file):
+            try:
+                rd_runner = calculator.Norm()
+                rd_runner.norm(self.__mosdepth_dir, sd_db, self.__threads)
+                rd_db = rd_runner.norm_db
+            except Exception as e:
+                QMessageBox.critical(self.__parent_form, "Calculating read depth failed", format_exc())
+                self.progress.emit(" Failed with: %s" % repr(e))
+                return
+        else:
+            if not path.exists(cn_file):
+                rd_loader = loader.BEDLoader()
+                rd_loader.load(rd_file)
+                rd_db = rd_loader.bed_db
+        self.progress.emit("")
+
+        # Step 04
+        self.progress.emit("Calculating CN")
+        gene_cn_file = path.join(self.__wrkdir, "04.gene_cn.txt")
+        cn_db = {}
+        if not path.exists(cn_file):
+            try:
+                cn_runner = calculator.CN()
+                cn_runner.convert(gc_file, rd_db, self.__threads)
+            except Exception as e:
+                QMessageBox.critical(self.__parent_form, "Calculating CN failed", format_exc())
+                self.progress.emit(" Failed with: %s" % repr(e))
+                return
+        else:
+            if not path.exists(gene_cn_file):
+                cn_loader = loader.BEDLoader()
+                cn_loader.load(cn_file)
+                cn_db = cn_loader.bed_db
+        self.progress.emit("")
+
+        # Step 05
+        self.progress.emit("Calculating gene CN")
+        round_cn_file = path.join(self.__wrkdir, "05.gene_cn_round.txt")
+        gene_cn_db = {}
+        if not path.exists(gene_cn_file):
+            try:
+                gene_loader = loader.GeneLoader()
+                gene_loader.load(self.__gene_list_file)
+                gene_bed_db = gene_loader.bed_db
+                gene_cn_runner = calculator.GeneCN()
+                gene_cn_runner.calc(cn_db, gene_bed_db)
+                gene_cn_db = gene_cn_runner.gene_cn
+            except Exception as e:
+                QMessageBox.critical(self.__parent_form, "Calculating gene CN failed", format_exc())
+                self.progress.emit(" Failed with: %s" % repr(e))
+                return
+        else:
+            if not path.exists(round_cn_file):
+                gene_cn_loader = loader.GeneCNLoader()
+                gene_cn_loader.load(gene_cn_file)
+                gene_cn_db = gene_cn_loader.gene_cn
+        self.progress.emit("")
+
+        # Step 06
+        self.progress.emit("Rounding CN")
+        rfd_dir = path.join(self.__wrkdir, "06.RFD")
+        round_cn_db = {}
+        if not path.exists(round_cn_file):
+            try:
+                round_cn_runner = calculator.RoundCN()
+                round_cn_runner.round(gene_cn_db)
+                round_cn_db = round_cn_runner.round_cn
+            except Exception as e:
+                QMessageBox.critical(self.__parent_form, "Rounding gene CN failed", format_exc())
+                self.progress.emit(" Failed with: %s" % repr(e))
+                return
+        else:
+            if not path.exists(rfd_dir):
+                round_cn_loader = loader.GeneCNLoader()
+                round_cn_loader.load(round_cn_file)
+                round_cn_db = round_cn_loader.gene_cn
+        self.progress.emit("")
+
+        # Step 07
+        self.progress.emit("RFD and F-test")
+        if not path.exists(rfd_dir):
+            try:
+                grp_loader = loader.GRPLoader()
+                grp_loader.load(self.__smp_file)
+                grp_db = grp_loader.grp_db
+                rfd_runner = calculator.RFD()
+                rfd_runner.calc(round_cn_db, grp_db, self.__wild_group)
+                rfd_db = rfd_runner.rfd_db
+            except Exception as e:
+                QMessageBox.critical(self.__parent_form, "Running RFD or F-test failed", format_exc())
+                self.progress.emit(" Failed with: %s" % repr(e))
+                return
+        self.progress.emit("Done")
+        self.completed.emit(1)
